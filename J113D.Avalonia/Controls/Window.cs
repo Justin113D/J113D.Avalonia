7b5eff48889d682b3355de829541d2b01ca388d9
Justin113D <Justin113D@gmail.com>
2024-09-04 21:02:44 +0200

    Multiple changes and additions Additions: - Custom window type with its own theme that supports hiding individual caption buttons - Adorner layer theme
    Changes:
    - Caption buttons no longer focussable and no longer tabstop
    - Removed fullscreen caption button
    - Menuitems no longer tabstop

---
+++ J113D.Avalonia/Controls/Window.cs
@@ -0,0 +1,48 @@
+﻿using Avalonia;
+using Avalonia.Platform;
+using System;
+
+namespace J113D.Avalonia.Controls
+{
+    public class Window : global::Avalonia.Controls.Window
+    {
+        public static readonly StyledProperty<bool> HideMinimizeButtonProperty =
+            AvaloniaProperty.Register<Window, bool>(nameof(HideMinimizeButton));
+
+        public static readonly StyledProperty<bool> HideCloseButtonProperty =
+            AvaloniaProperty.Register<Window, bool>(nameof(HideCloseButton));
+
+        public static readonly StyledProperty<bool> HideRestoreButtonProperty =
+            AvaloniaProperty.Register<Window, bool>(nameof(HideRestoreButton));
+
+        public bool HideMinimizeButton
+        {
+            get => GetValue(HideMinimizeButtonProperty);
+            set => SetValue(HideMinimizeButtonProperty, value);
+        }
+
+        public bool HideCloseButton
+        {
+            get => GetValue(HideCloseButtonProperty);
+            set => SetValue(HideCloseButtonProperty, value);
+        }
+
+        public bool HideRestoreButton
+        {
+            get => GetValue(HideRestoreButtonProperty);
+            set => SetValue(HideRestoreButtonProperty, value);
+        }
+
+        protected override Type StyleKeyOverride => typeof(Window);
+
+        public Window() : base()
+        {
+
+        }
+
+        public Window(IWindowImpl impl) : base(impl)
+        { 
+        
+        }
+    }
+}
