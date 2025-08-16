1146781b23e557afe387483db34d6f1693d4f9ef
Justin113D <Justin113D@gmail.com>
2025-08-17 00:19:18 +0200

    Added data validation

---
+++ src/J113D.Avalonia/Converters/ExceptionConverter.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace J113D.Avalonia.Converters
+{
+	/// <summary>
+	/// Value convert for converting an exception to a text
+	/// </summary>
+	public class ExceptionConverter : IValueConverter
+	{
+		/// <inheritdoc/>
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if(value is string)
+			{
+				return value;
+			}
+			else if(value is Exception exception)
+			{
+				return exception.Message;
+			}
+			else if(value is Array array && array.Length > 0)
+			{
+				return Convert(array.GetValue(0), targetType, parameter, culture);
+			}
+
+			return "An unknown error occured";
+		}
+
+		/// <inheritdoc/>
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
